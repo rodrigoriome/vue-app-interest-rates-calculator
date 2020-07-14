7a9a217deb54b53ea498dcbc3a7ea3ff07f9012f
Rodrigo Mesquita <rodrigoriome@gmail.com>
2020-07-14 00:45:44 -0300

    Add build tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    extends: [
+        "plugin:vue/essential",
+        "eslint:recommended",
+        "prettier",
+        "@vue/prettier",
+    ],
+    parserOptions: {
+        parser: "babel-eslint",
+        sourceType: "module",
+    },
+    plugins: [
+        "prettier",
+    ],
+    rules: {
+        "prettier/prettier": "error",
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    },
 };
